3d177f5b49aee7fbe7ad7716da24006b0e7791ee
brandford7 <brandfordjunior@gmail.com>
2026-03-12 16:09:20 +0000

    changed synchronize to true to create table for league

--- src/app.module.ts
+++ src/app.module.ts
@@ -30,7 +30,7 @@ import { LeagueModule } from './league/league.module';
         database: configService.get('DB_NAME'),
         url: configService.get('DB_URL'),
         entities: [__dirname + '/**/*.entity{.ts,.js}'],
-        synchronize: configService.get('NODE_ENV') === 'development',
+        synchronize: true,
         logging: configService.get('NODE_ENV') === 'development',
       }),
       inject: [ConfigService],
